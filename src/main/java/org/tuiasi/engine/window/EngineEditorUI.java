dfed8c2e71b1e61d497066e02f3426b784a756b7
danelthedev <danelthedev@gmail.com>
2023-11-15 00:55:45 +0200

    minor changes

--- src/main/java/org/tuiasi/engine/window/EngineEditorUI.java
+++ src/main/java/org/tuiasi/engine/window/EngineEditorUI.java
@@ -1,26 +1,25 @@
 package org.tuiasi.engine.window;
 
 import imgui.ImGui;
+import imgui.flag.ImGuiWindowFlags;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 public class EngineEditorUI {
 
     private boolean showText = false;
 
     public void renderUI(){
-        ImGui.begin("Hello, world!"); // Start the Dear ImGui frame
-
-        if(ImGui.button("Im a banana")) {
-            showText = true;
-            ImGui.sameLine();
-            if(ImGui.button("Im a potato")) {
-                showText = false;
-            }
-        }
-
-        if(showText) {
-            ImGui.text("I'm a text label!");
-        }
+        //create a imgui window that has fixed size and sticks to the top left corner
+        //set position to 0,0 and size to 300,300
+        ImGui.begin("Hello, world!", ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoCollapse);
+        ImGui.setWindowPos(0,0);
+        ImGui.setWindowSize(300,300);
+        ImGui.text("This is some useful text.");               // Display some text (you can use a format strings too)
+        ImGui.checkbox("Demo Window", showText);      // Edit bools storing our window open/close state
 
         ImGui.end();
+
     }
 }
